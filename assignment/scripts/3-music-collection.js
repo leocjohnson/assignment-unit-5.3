87fd9bee161a3a4e1c16628cb121050728178e28
Leo Johnson <leochinonejohnson@gmail.com>
2023-10-18 21:17:33 -0500

    added records to collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,30 +2,50 @@ console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
 let myCollection = [];
-
-console.log('Here is my record collection! Take a look:');
+console.log('Here is my record collection box! Take a look below:');
 console.log(myCollection);
 
 function addToCollection(collection, title, artist, yearPublished){
   if(collection===myCollection){
-    let newRecord = {
+    let record = {
       title: title,
       artist: artist,
       yearPublished: yearPublished
     }
-    myCollection.push(newRecord);
-    return newRecord;
+    myCollection.push(record);
+    console.log('Added', title, 'to my collection!');
+    return record;
   }else{
-    console.log('No such collection. Try again!');
+    console.log('Not my collection. Try again!');
     return false;
   }
 }
 
-addToCollection(myCollection, 'Flood', 'Boris', 2000);
+console.log(addToCollection(myCollection, 'Only A Lad', 'Oingo Boingo', 1981));
+console.log(addToCollection(myCollection, 'System Of A Down', 'System Of A Down', 1998));
+console.log(addToCollection(myCollection, 'Hybrid Theory', 'Linkin Park', 2000));
+console.log(addToCollection(myCollection, 'Tired Of Tomorrow', 'Nothing', 2016));
+console.log(addToCollection(myCollection, 'Hold Your Horse Is', 'Hella', 2002));
+console.log(addToCollection(myCollection, 'Fantastic Planet', 'Failure', 1996));
+console.log(addToCollection(myCollection, 'Hey What', 'Low', 2021));
+console.log(addToCollection(myCollection, 'I Could Live In Hope', 'Low', 1994));
+console.log(addToCollection(myCollection, 'Akuma No Uta', 'Boris', 2003));
+console.log(addToCollection(myCollection, 'Bleach', 'Nirvana', 1989));
+console.log(addToCollection(myCollection, 'In Utero', 'Nirvana', 1993));
+console.log(addToCollection(myCollection, 'H.A.Q.Q.', 'Liturgy', 2019));
+console.log(addToCollection(myCollection, 'The Ark Work', 'Liturgy', 2015));
+console.log(addToCollection(myCollection, 'Loveless', 'My Bloody Valentine', 1991));
+console.log(addToCollection(myCollection, 'Isn\'t Anything', 'My Bloody Valentine', 1988));
+console.log(addToCollection(myCollection, 'A Brief Memoriam', 'Frail Body', 2019));
+console.log(addToCollection(myCollection, 'Blake Mills', 'Blake Mills', 2010));
+console.log(myCollection);
 
-
-
-
+function showCollection(collection){
+  while(collection){
+    console.log(record.yearPublished + ':', record.title, 'by', record.artist + '.');
+  }
+}
+showCollection(myCollection);
 
 console.log(' --- test code below --- ');
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
